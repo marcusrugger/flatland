594d8505b1005ecc95fcb3b04bc05dd674e44382
Marcus Rugger <marcusrugger@yahoo.com>
2016-04-30 20:55:33 -0400

    Update

--- src/color.cs
+++ src/color.cs
@@ -8,6 +8,7 @@ interface Color
     byte Red   { get; }
     byte Green { get; }
     byte Blue  { get; }
+    byte Alpha { get; }
 }
 
 
@@ -23,10 +24,40 @@ class ColorRGB : Color
         this.green = green;
         this.blue  = blue;
     }
+    
+    public ColorRGB(Color other)
+    {
+        this.red   = other.Red;
+        this.green = other.Green;
+        this.blue  = other.Blue;
+    }
 
     public byte Red   { get { return red;   } }
     public byte Green { get { return green; } }
     public byte Blue  { get { return blue;  } }
+    public byte Alpha { get { return 255;   } }
+}
+
+
+class ColorRGBA : Color
+{
+    byte red;
+    byte green;
+    byte blue;
+    byte alpha;
+
+    public ColorRGBA(byte red, byte green, byte blue, byte alpha)
+    {
+        this.red   = red;
+        this.green = green;
+        this.blue  = blue;
+        this.alpha = alpha;
+    }
+
+    public byte Red   { get { return red;   } }
+    public byte Green { get { return green; } }
+    public byte Blue  { get { return blue;  } }
+    public byte Alpha { get { return alpha; } }
 }
 
 
---
+++ src/color.test.cs
@@ -0,0 +1,83 @@
+using NUnit.Framework;
+using System;
+using Flatland;
+
+
+[TestFixture]
+class ColorTest
+{
+    [Test]
+    public void ColorRGB()
+    {
+        Color color = new ColorRGB(1, 2, 3);
+
+        Assert.That(color.Red  , Is.EqualTo(1));
+        Assert.That(color.Green, Is.EqualTo(2));
+        Assert.That(color.Blue , Is.EqualTo(3));
+        Assert.That(color.Alpha, Is.EqualTo(255));
+    }
+
+    [Test]
+    public void ColorRGB_From_ColorRGB()
+    {
+        ColorRGB original = new ColorRGB(1, 2, 3);
+        Color    color    = new ColorRGB(original);
+
+        Assert.That(color.Red  , Is.EqualTo(   1 ));
+        Assert.That(color.Green, Is.EqualTo(   2 ));
+        Assert.That(color.Blue , Is.EqualTo(   3 ));
+        Assert.That(color.Alpha, Is.EqualTo( 255 ));
+
+        Assert.That(original.Red  , Is.EqualTo(   1 ));
+        Assert.That(original.Green, Is.EqualTo(   2 ));
+        Assert.That(original.Blue , Is.EqualTo(   3 ));
+        Assert.That(original.Alpha, Is.EqualTo( 255 ));
+    }
+
+    [Test]
+    public void ColorRGB_From_ColorRGBA()
+    {
+        ColorRGBA original = new ColorRGBA(1, 2, 3, 4);
+        Color     color    = new ColorRGB(original);
+
+        Assert.That(color.Red  , Is.EqualTo(   1 ));
+        Assert.That(color.Green, Is.EqualTo(   2 ));
+        Assert.That(color.Blue , Is.EqualTo(   3 ));
+        Assert.That(color.Alpha, Is.EqualTo( 255 ));
+
+        Assert.That(original.Red  , Is.EqualTo( 1 ));
+        Assert.That(original.Green, Is.EqualTo( 2 ));
+        Assert.That(original.Blue , Is.EqualTo( 3 ));
+        Assert.That(original.Alpha, Is.EqualTo( 4 ));
+    }
+
+    [Test]
+    public void ColorRGBA()
+    {
+        Color color = new ColorRGBA(1, 2, 3, 4);
+
+        Assert.That(color.Red  , Is.EqualTo( 1 ));
+        Assert.That(color.Green, Is.EqualTo( 2 ));
+        Assert.That(color.Blue , Is.EqualTo( 3 ));
+        Assert.That(color.Alpha, Is.EqualTo( 4 ));
+    }
+
+    [Test]
+    public void ColorsTest()
+    {
+        Assert.That(Colors.Red.Red    , Is.EqualTo( 255 ));
+        Assert.That(Colors.Red.Green  , Is.EqualTo(   0 ));
+        Assert.That(Colors.Red.Blue   , Is.EqualTo(   0 ));
+        Assert.That(Colors.Red.Alpha  , Is.EqualTo( 255 ));
+
+        Assert.That(Colors.Green.Red  , Is.EqualTo(   0 ));
+        Assert.That(Colors.Green.Green, Is.EqualTo( 255 ));
+        Assert.That(Colors.Green.Blue , Is.EqualTo(   0 ));
+        Assert.That(Colors.Green.Alpha, Is.EqualTo( 255 ));
+
+        Assert.That(Colors.Blue.Red   , Is.EqualTo(   0 ));
+        Assert.That(Colors.Blue.Green , Is.EqualTo(   0 ));
+        Assert.That(Colors.Blue.Blue  , Is.EqualTo( 255 ));
+        Assert.That(Colors.Blue.Alpha , Is.EqualTo( 255 ));
+    }
+}
